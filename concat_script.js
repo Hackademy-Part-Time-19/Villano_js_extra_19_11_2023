8c9cefef8830c97589e82ef94b9b33d7ad83bb9a
Antonio_V <antoniovillano91@hotmail.it>
2023-11-19 12:06:44 +0100

    concat

---
+++ concat_script.js
@@ -0,0 +1,16 @@
+let array = ["Franco", "Gennaro", "Valerio", "Giulia", "Eugenio" ]
+array_1=[
+    ['un','per','incatenarli.'],
+    ['Anello', 'trovarli, '],
+    ['ghermirli','e'],
+    ['gondor', 'mark'],]
+    array_2 = [
+        [['trovarli']],
+        ['tu, ', 'sciocchi'],
+        ['tu, ', 'sciocchi',['padron','Sauron']],
+        ['nel', ['fuggite', 'gandalf']],
+        [['domarli,', 'passare'], 'buio']]
+const arr1 = [1, 2, [3, 4]];
+const arr2 = [[5, 6], 7, 8];
+const arr3 = array.concat(arr1,array_1,arr2,array_2);
+console.log(arr3)
